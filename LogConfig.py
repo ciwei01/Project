6e3d1aecff762d8be638fbd3ad37240d4736519f
ciwei01 <290729752@qq.com>
2019-10-30 14:11:22 +0800

    add logConfig.py

---
+++ LogConfig.py
@@ -0,0 +1,25 @@
+import datetime
+import logging
+import os 
+
+
+class LogConfig:
+    @staticmethod
+    def init(path):
+        global logger
+
+        if not os.path.exists('./log'):
+            os.mkdir('./log')
+
+        nowTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        logging.basicConfig(level=logging.INFO,
+                            format='%(asctime)s %(levelname)s %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S',
+                            filename = './log/'+nowTime+'.log',
+                            filemode='a')
+        logger = logging.getLogger()
+
+
+    @staticmethod
+    def getLogger():
+        return logger
